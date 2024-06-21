58412f2fce11721565dcc0366edc498bc4f9b51d
caioo <matossousacaio.cms@gmail.com>
2024-06-21 14:55:24 -0300

    coisa nova

---
+++ Aulas Udemy/Exemplo14Heranca/conta/src/entities/Account.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    protected Double balance;
+    
+    public Account(){
+    }
+
+    public Account(Integer number, String holder, Double balance){
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+    
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+    public String getHolder() {
+        return holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void saqque(double amount){
+        balance -= amount + 5.0;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+}
